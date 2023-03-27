6d010d0f2185e7c5b51821c6aafc640d88c686b6
Sinane <sin.sim@outlook.fr>
2023-03-27 22:12:24 +0200

    feat: verify ownership function

--- index.js
+++ index.js
@@ -1,23 +1,41 @@
 const { TezosToolkit } = require("@taquito/taquito");
 const axios = require("axios");
 
-
 // Fonction pour récupérer les NFTs d'un wallet
 const WALLET_ADDRESS = "tz1eQzGZXy36xx6xDT1Xp6dUdHS2dXowKdCq";
 
+const tzkt = axios.create({
+  baseURL: "https://api.tzkt.io/v1/",
+});
+
 const getWalletNFTs = async () => {
   const Tezos = new TezosToolkit("https://rpc.tzbeta.net/");
-  const lastBlock = (await Tezos.rpc.getBlock()).header.level;
-  console.log(lastBlock);
-  const walletData = axios.get(
-    `https://api.tzkt.io/v1/tokens/historical_balances/${lastBlock}?token.standard=fa2&account=${WALLET_ADDRESS}`
+
+  const walletData = tzkt.get(
+    `tokens?token.standard=fa2&account=${WALLET_ADDRESS}`
   );
 
   return walletData;
 };
 
-getWalletNFTs()
-  .then((data) => {
+const verifyOwnership = async (walletAddress, contractAddress) => {
+  const Tezos = new TezosToolkit("https://rpc.tzbeta.net/");
+  const lastBlock = (await Tezos.rpc.getBlock()).header.level;
+  
+  const endpoint = `tokens/historical_balances/${lastBlock}?account=${walletAddress}&token.standard=fa2&token.contract=${contractAddress}`;
+  const walletData = tzkt.get(endpoint);
+
+  return walletData;
+};
+
+// getWalletNFTs()
+//   .then((data) => {
+//     console.log(data);
+//   })
+//   .catch((err) => console.log(err.message));
+
+verifyOwnership(WALLET_ADDRESS, "KT1NVvPsNDChrLRH5K2cy6Sc9r1uuUwdiZQd")
+  .then(({ data }) => {
     console.log(data);
   })
   .catch((err) => console.log(err.message));
